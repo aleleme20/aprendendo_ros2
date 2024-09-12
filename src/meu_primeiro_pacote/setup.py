f1bcc8780e7772d953bab41e6677a92f967f09ef
aleleme20 <aleamaral.leme@gmail.com>
2024-09-12 22:01:34 +0000

    desvio de obstáculo

--- src/meu_primeiro_pacote/setup.py
+++ src/meu_primeiro_pacote/setup.py
@@ -25,6 +25,7 @@ setup(
             'talker = meu_primeiro_pacote.talker:main',
             'listener = meu_primeiro_pacote.listener:main',
             'R2D2 = meu_primeiro_pacote.R2D2:main',
+            'r2d2_obstaculo = meu_primeiro_pacote.r2d2_obstaculo:main'
         ],
     },
 )
