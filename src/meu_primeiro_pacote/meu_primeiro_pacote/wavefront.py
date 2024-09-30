81a49fa70dd2110542e12e371c7b6bd62acd6b8b
aleleme20 <aleamaral.leme@gmail.com>
2024-09-30 21:42:55 +0000

    atividade wavefront - 1

--- src/meu_primeiro_pacote/meu_primeiro_pacote/wavefront.py
+++ src/meu_primeiro_pacote/meu_primeiro_pacote/wavefront.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
 
-pgmf = open('my_map.pgm', 'rb')
+pgmf = open('src/my_map.pgm', 'rb')
 matrix = plt.imread(pgmf)
 print (matrix)
 
-# matrix = 1.0 * (matrix > 250)
+matrix = 1.0 * (matrix > 250)
+matrix [100][800] = 0 #fica preto
+matrix [100][700] = 2 #define coordenadas do objetivo final
 plt.imshow(matrix, interpolation='nearest', cmap='gray')
 plt.show()
-
